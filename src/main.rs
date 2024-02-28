ad888f2c7b86aa445859075a8da429b820497231
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2024-02-27 22:07:22 -0400

    added basic menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,55 @@
 mod task;
+use std::{
+    default,
+    io::{self, Write},
+};
+use task::Task;
+
+fn menu() {
+    println!("\nTodo List Menu:\n1. Show all tasks\n2. Add a task\n3. Edit a task\n4. Remove a task\n5. Exit");
+}
 
 fn main() {
-    println!("Hello, world!");
+    let mut todo_list: Vec<Task> = Vec::new();
+
+    loop {
+        menu();
+        println!("Please select an option:  ");
+        io::stdout().flush().unwrap();
+
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+        let choice: usize = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        match choice {
+            1 => {
+                // print tasks in list
+            }
+            2 => {
+                // add a task
+            }
+            3 => {
+                // edit a task
+            }
+            4 => {
+                // remove a task
+            }
+            5 => {
+                // quit
+                println!("Goodbye!");
+                break;
+            }
+            _ => {
+                println!("Please select a number between 1 and 5");
+            }
+        }
+    }
 }
